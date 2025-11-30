761115cee86b782bf862bc47f8b0344c538df30a
William LeGates <wl10794313@sju.edu>
2025-11-30 08:29:49 -0500

    Adding necessary files to Repo

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App extends Customer {
+    public static void main(String[] args) throws Exception {
+
+        Scanner k = new Scanner(System.in);
+
+        System.out.println("Welcome.\n Would you like to:\n1. Create a new Accout\n2. Change Information");
+
+        int ans = k.nextInt();
+
+        switch(ans){
+            case(1):
+            
+
+        }
+
+    }
+}
---
+++ src/Customer.java
@@ -0,0 +1,56 @@
+public class Customer{
+    private int age;
+    private String firstName;
+    private char midInit;
+    private String lastName;
+
+    public Customer(){
+        age = this.age;
+        firstName = this.firstName;
+        midInit = this.midInit;
+        lastName = this.lastName;
+    }
+
+    public Customer(int a, String f, char m, String l){
+        age = a;
+        firstName = f;
+        midInit = m;
+        lastName = l;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setFirstName(String f){
+        firstName = f;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setMidInit(char m){
+        midInit = m;
+    }
+
+    public char getMidInit(){
+        return midInit;
+    }
+
+    public void setLastName(String l){
+        lastName = l;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String toString(){
+        return "age: " + age + "First name: " + firstName + "Middle initial : " + midInit + "Last name: " + lastName;
+    }
+}
